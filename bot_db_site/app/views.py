192de5885ae740650a6ce21e07e662f0cbfddea2
klordo <gklimg@gmail.com>
2023-09-30 22:23:12 +0000

    edit backend

--- bot_db_site/app/views.py
+++ bot_db_site/app/views.py
@@ -2,43 +2,48 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import TelegramChat
+from .serializers import TelegramChatSerializer
 from django.shortcuts import get_object_or_404
+import datetime
 import pytz
 
 class TelegramChatView(APIView):
 
-    def get(self, request, telegram_chat_id, format=None):
-        telegram_chat = get_object_or_404(TelegramChat, telegram_chat_id=telegram_chat_id)
-        data = {
-            "telegram_chat_id": telegram_chat_id,
-            "answers": telegram_chat.answers,
-            "questions": telegram_chat.questions,
-            "time": telegram_chat.timestamp.astimezone(pytz.timezone('Asia/Irkutsk')).strftime('%Y-%m-%d %H:%M:%S')
-        }
-        return Response(data, status=status.HTTP_200_OK)
-    
-    def post(self, request, format=None):
-        telegram_chat_id = request.data.get("telegram_chat_id")
-        text = request.data.get("question") or request.data.get("answer")
-        
-        if not telegram_chat_id or not text:
-            return Response(
-                {"error": "Both telegram_chat_id and either question or answer are required."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+    def get(self, request, chat_id):
+        print(request.META.get('REMOTE_ADDR'))
+        try:
+            chat = TelegramChat.objects.get(chat_id=chat_id)
+            serializer = TelegramChatSerializer(chat)
+            return Response(serializer.data)
+        except TelegramChat.DoesNotExist:
+            return Response({"message": f"chat with chat_id={chat_id} not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        telegram_chat, created = TelegramChat.objects.get_or_create(telegram_chat_id=telegram_chat_id)
+    def post(self, request):
+        print(request.META.get('REMOTE_ADDR'))
+        data = request.data
+
+        time_now = datetime.datetime.now().astimezone(pytz.timezone('Asia/Irkutsk')).strftime('%d-%m-%Y %H:%M:%S')
         
-        if "question" in request.data:
-            telegram_chat.questions.append(text)
-        if "answer" in request.data:
-            telegram_chat.answers.append(text)
-        
-        telegram_chat.save()
-        
-        return Response({"message": "Record updated successfully."}, status=status.HTTP_200_OK)
+        chat_history_item = {
+            'type': data.get('type', ''),
+            'text': data.get('text', ''),
+            'time': time_now,
+        }
+
+        chat_id = data['chat_id']
+        chat, created = TelegramChat.objects.get_or_create(chat_id=chat_id)
+
+        chat.chat_history.append(chat_history_item)
+        chat.save()
+
+        return Response({"message": f"successfully"}, status=status.HTTP_201_CREATED)
+
     
-    def delete(self, request, telegram_chat_id, format=None):
-        telegram_chat = get_object_or_404(TelegramChat, telegram_chat_id=telegram_chat_id)
-        telegram_chat.delete()
-        return Response({"message": "Record deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, chat_id):
+        print(request.META.get('REMOTE_ADDR'))
+        try:
+            chat = TelegramChat.objects.get(chat_id=chat_id)
+            chat.delete()
+            return Response({"message": "successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except TelegramChat.DoesNotExist:
+            return Response({"message": f"chat with chat_id={chat_id} not found"}, status=status.HTTP_404_NOT_FOUND)
